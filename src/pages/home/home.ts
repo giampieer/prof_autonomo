e0c98425727c1d9b35d50956a6ad588e01807138
giampieer <giampieer24@gmail.com>
2018-09-05 19:45:25 -0500

    init proyect

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {InAppBrowser, InAppBrowserOptions} from "@ionic-native/in-app-browser";
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+  url: string;
+  constructor(private inAppBrowser: InAppBrowser) {
+    this.openWebpage('http://192.168.43.1:8080/profesora/');
   }
-
+  openWebpage(url: string) {
+    const options: InAppBrowserOptions = {
+      zoom: 'no'
+    }
+    // Opening a URL and returning an InAppBrowserObject
+    const browser = this.inAppBrowser.create(url, '_self', options);
+    // Inject script
+  }
 }
+
